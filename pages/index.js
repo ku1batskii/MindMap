6072c5b2d30106383569c5e1f208393220293bf2
ku1batskii <168655448+ku1batskii@users.noreply.github.com>
2026-03-14 15:44:22 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import dynamic from "next/dynamic";
-const MindMap = dynamic(() => import("../components/MindMap"), { ssr: false });
+const MindMap = dynamic(() => import("../components/MindMap.js"), { ssr: false });
 export default function Home() {
   return <MindMap />;
 }
