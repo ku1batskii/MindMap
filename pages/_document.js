97e9a388e298bc3b2bdb3539efe387b8e3fd4947
ku1batskii <168655448+ku1batskii@users.noreply.github.com>
2026-03-18 17:03:48 +0100

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,31 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="ru">
+      <Head>
+        {/* PWA manifest */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* iOS PWA support */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="MindMap" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+
+        {/* Theme color */}
+        <meta name="theme-color" content="#090909" />
+
+        {/* Prevent zoom on input focus (iOS) */}
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
+
+        {/* Splash screens for iPhone */}
+        <meta name="mobile-web-app-capable" content="yes" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
